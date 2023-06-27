32389f79235082ccab15093f580d31fc6fb1d138
Chris <jeky.tck@gmail.com>
2023-06-27 22:21:50 +0100

    getting inputs and assigning num of zones to variables

--- RNG_analysis/module.py
+++ RNG_analysis/module.py
@@ -3,6 +3,8 @@ import numpy as np
 from RNG_analysis.validation import *
 from RNG_analysis.categorizer import *
 
+def get_num_of_zones():
+    return int(input("Enter the number of zones: "))
 
 def generate_random_numbers(n):
     if validate_positive_integer(n):
@@ -13,6 +15,8 @@ def main():
     # main function to interact with module
     n = int(input("Enter the number of random numbers to generate: "))
     random_numbers = generate_random_numbers(n)
+    num_of_zones = get_num_of_zones()
+    zone_range = define_zones_range(num_of_zones, random_numbers.size)
     print(f"Generated {n} random numbers: {random_numbers}")
 
 if __name__ == "__main__":
