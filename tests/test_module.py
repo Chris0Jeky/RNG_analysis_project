eb4da05344ce14ad7c8b69f4de100d4fd189f26a
Chris <jeky.tck@gmail.com>
2023-06-27 18:05:53 +0100

    Basic tests added

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,10 +1,19 @@
-import unittest
+# tests/test_module.py
+
+import numpy as np
+from RNG_analysis.module import generate_random_numbers
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+def test_generate_random_numbers():
+    n = 10
+    random_numbers = generate_random_numbers(n)
 
+    # Test that the function returns a numpy array
+    assert isinstance(random_numbers, np.ndarray)
 
-if __name__ == '__main__':
-    unittest.main()
+    # Test that the length of the array is correct
+    assert len(random_numbers) == n
+
+    # Test that all numbers are between 0 and 1
+    assert np.all((random_numbers >= 0) & (random_numbers < 1))
+
