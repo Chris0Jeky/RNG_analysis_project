38e58dc42a737b174ad339dd9ed9e6822bd1ce0e
Chris <jeky.tck@gmail.com>
2023-06-28 18:34:02 +0100

    modified logic. zone_range based on max value instead of length

--- RNG_analysis/categorizer.py
+++ RNG_analysis/categorizer.py
@@ -1,5 +1,5 @@
-def define_zones_range(number_of_zones, length_of_array):
-    zone_range = round(length_of_array / number_of_zones)
+def define_zones_range(number_of_zones, max_value):
+    zone_range = round(max_value / number_of_zones)
     return zone_range
 
 
